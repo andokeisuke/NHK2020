ad021ba11e954d3a112590927e3ae1b7a2e91de0
andokeisuke <kakipi.moai@gmail.com>
2019-10-06 15:32:52 +0900

    tablet3

---
+++ src/control/src/control.cpp
@@ -0,0 +1,113 @@
+#include <ros/ros.h>
+#include <sensor_msgs/Joy.h>
+#include <std_msgs/Int8.h>
+#include <std_msgs/Int16.h>
+#include <std_msgs/Bool.h>
+
+#include <geometry_msgs/Twist.h>
+
+
+
+
+#define MAX_VEL 			1.0//m/s //launchファイルでなにも指定しなかったときの値
+#define MIN_VEL 			-1.0//m/s
+#define MAX_ANGULAR_VEL 	1.0//m/s
+#define MIN_ANGULAR_VEL 	-1.0//m/s
+
+ros::Publisher twist_pub;
+
+ros::Subscriber joy_sub;
+
+
+	std_msgs::Int16 deg;
+
+
+
+float map(float x, float in_min, float in_max, float out_min, float out_max);
+
+class Joystick
+{
+public:
+	float linear_x;
+	float linear_y;
+	float right_spin;
+	float left_spin;
+	float shagai_shoot;
+
+};
+
+Joystick Joystick;
+
+void joy_Callback(const sensor_msgs::Joy& joy){
+
+  Joystick.linear_x = -joy.axes[0];//左のスティック上下
+  Joystick.linear_y = joy.axes[1];//左のスティック左右
+
+  Joystick.right_spin = joy.buttons[4];//L2ボタン(コントローラの5番)
+  Joystick.left_spin  = joy.buttons[5];//R2ボタン(コントローラの6番)
+
+
+
+  geometry_msgs::Twist twist;
+  
+  twist.linear.x =  Joystick.linear_x;
+  twist.linear.y =  Joystick.linear_y;
+  //twist.linear.x = map(Joystick.linear_x,-1,1,MIN_VEL,MAX_VEL);
+  //twist.linear.y = map(Joystick.linear_y,-1,1,MIN_VEL,MAX_VEL);
+
+  if(Joystick.right_spin == 1)
+  {
+	twist.angular.z = MIN_ANGULAR_VEL;
+  }    
+  else if(Joystick.left_spin == 1)
+  {
+	twist.angular.z = MAX_ANGULAR_VEL;
+  }
+
+  twist_pub.publish(twist);
+
+}
+
+
+float map(float x, float in_min, float in_max, float out_min, float out_max)
+{//arduinoのmap関数と同じ
+	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "joy");
+
+	ros::NodeHandle n;
+	ros::NodeHandle pn("~");
+
+	float MAX_VEL_param=MAX_VEL;
+	float MIN_VEL_param=MIN_VEL;
+	float MAX_ANGULAR_VEL_param=MAX_ANGULAR_VEL;
+	float MIN_ANGULAR_VEL_param=MIN_ANGULAR_VEL;
+
+	pn.getParam("MAX_VEL",MAX_VEL_param);
+	pn.getParam("MIN_VEL",MIN_VEL_param);
+	pn.getParam("MAX_ANGULAR_VEL",MAX_ANGULAR_VEL_param);
+	pn.getParam("MIN_ANGULAR_VEL",MIN_ANGULAR_VEL_param);
+
+	joy_sub = n.subscribe("joy", 1, joy_Callback);
+	
+	twist_pub = n.advertise<geometry_msgs::Twist>("sub",1);
+	
+
+
+	ros::Rate r(10.0);
+
+	while(n.ok()){
+
+		ros::spinOnce();
+
+		r.sleep();
+
+
+
+	}
+
+}
---
+++ src/odometry/src/odometry.cpp
@@ -0,0 +1,88 @@
+#include <ros/ros.h>  // rosで必要はヘッダーファイル
+#include <std_msgs/Int64.h>
+#include <geometry_msgs/Twist.h> // ロボットを動かすために必要
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_datatypes.h>
+#include <gazebo_msgs/ModelStates.h> 
+#include <sensor_msgs/JointState.h>
+#include <iostream>
+
+    double state_odom_x=0.0;//オドメトリX座標[m]
+    double state_odom_y=0.0;//オドメトリY座標[m]
+    double state_odom_th=0.0; //オドメトリ姿勢[rad]
+    double predistance = 0;
+    double x = 0;
+    double y  = 0;
+    double w = 0;
+    double r = 0;
+    double th = 0;
+geometry_msgs::Quaternion odom_quat ;
+void twist_Callback(const geometry_msgs::Twist::ConstPtr& twist)
+   {
+	x = twist->linear.x;
+	y = twist->linear.y;
+	w = twist->angular.z;
+        r = sqrt(x*x+y*y);
+
+
+
+   }
+
+void enc_Callback(const std_msgs::Int64::ConstPtr& enc)
+   {
+
+	double left_rear_enc = enc->data; // 右車軸の位置[rad]
+	double distance = left_rear_enc/2024*M_PI*0.064;		
+
+     //if(distance-predistance>=0){
+     if(w == 0&&r !=0){//回転せず止まっていない時
+     	state_odom_x=state_odom_x+(distance-predistance)/r*x;
+     	state_odom_y=state_odom_y+ (distance-predistance)/r*y;
+     }
+    th =th+ (distance-predistance) * w/0.15;
+     odom_quat = tf::createQuaternionMsgFromYaw(th);
+
+
+	predistance  = distance;
+     //}
+
+   }
+
+
+
+
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "odom");
+    ros::NodeHandle n;
+    tf::TransformBroadcaster odom_broadcaster;
+    ros::Time current_time;
+    current_time = ros::Time::now();
+    ros::Subscriber enc_sub = n.subscribe<std_msgs::Int64>("/enc", 1, enc_Callback);
+    ros::Subscriber twist_sub = n.subscribe<geometry_msgs::Twist>("/sub", 1, twist_Callback);
+    ros::Publisher odm_pub = n.advertise<geometry_msgs::TransformStamped>("/odometry",1000);
+    ros::Rate r(30.0);
+    geometry_msgs::TransformStamped odom_trans;
+    odom_trans.header.frame_id = "odom";
+    odom_trans.child_frame_id = "base_link";
+
+    while(n.ok()){
+
+    current_time = ros::Time::now();
+    
+    odom_trans.header.stamp = current_time;
+
+    ros::spinOnce();
+    odom_trans.transform.translation.x = state_odom_x;
+    odom_trans.transform.translation.y = state_odom_y;
+    odom_trans.transform.translation.z = 0.0;
+
+    odom_trans.transform.rotation = odom_quat;
+    odom_broadcaster.sendTransform(odom_trans);
+    odm_pub.publish(odom_trans);
+
+    ros::spinOnce();
+    r.sleep();
+    }
+}
